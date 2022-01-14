bc749c450d6220149f11a093c136fc26e96e3c1d
Casper Yonel <yonelcasper@gmail.com>
2022-01-14 14:09:39 -0700

    updated

--- server/server.js
+++ server/server.js
@@ -4,16 +4,10 @@ const app = express()
 const cors = require('cors')
 const { SERVER_PORT } = process.env
 const { seed } = require('./seed')
-
 app.use(express.json())
 app.use(cors())
 
-
-
 // Seed:
 app.post('/seed', seed)
- 
-
-
 
 app.listen(SERVER_PORT, () => console.log(`Listening on Port ${SERVER_PORT}`))
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div >
-      <h1>hgwå</h1>
+      
     </div>
   );
 }
