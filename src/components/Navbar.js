e6c3569b170e3d8a39b73686fd9ce89c2f06471d
Casper Yonel <yonelcasper@gmail.com>
2022-01-15 10:44:00 -0700

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
  
+import navbar from './navbar.scss'
 import logo from "../assets/reit-visuals/monopoly-man-color-2.png"
 import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements'
-import navbar from './navbar.scss'
 
 // import component home pages from /pages folder
 
@@ -14,8 +14,8 @@ const Navbar = () => {
         <>
             <Nav>
                 <NavLink to='/'>
-                    {/* <img src={logo} alt="" /> */}
-                    <h1>logo</h1>
+                    <img id='logo' src={logo} alt="logo" />
+                    {/* <h1>logo</h1> */}
                 </NavLink>
                 <Bars />
                 <NavMenu>
@@ -23,10 +23,11 @@ const Navbar = () => {
                    <NavLink to='/howtoplay' activeStyle>How To Play</NavLink>
                    <a className='external-nav-element' href="https://medium.com/@REITDAO" target='_blank' rel="noopener noreferrer">Blog</a>
                    <a className='external-nav-element' href="https://medium.com/@REITDAO" target='_blank' rel="noopener noreferrer">Discord</a>
+                   <NavBtnLink to='/ido'>IDO Whitelist</NavBtnLink>
                 </NavMenu>
-                <NavBtn>
+                {/* <NavBtn>
                     <NavBtnLink to='/ido'>IDO Whitelist</NavBtnLink>
-                </NavBtn>
+                </NavBtn> */}
             </Nav>
         </>
     )
--- src/components/NavbarElements.js
+++ src/components/NavbarElements.js
@@ -9,8 +9,6 @@ export const Nav = styled.nav`
   justify-content: space-between;
   padding: 0.5rem calc((100vw - 1000px) / 2);
   z-index: 10;
-  /* Third Nav */
-  /* justify-content: flex-start; */
 `;
 
 export const NavLink = styled(Link)`
@@ -29,6 +27,7 @@ export const NavLink = styled(Link)`
 export const Bars = styled(FaBars)`
   display: none;
   color: #fff;
+
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
@@ -43,12 +42,8 @@ export const Bars = styled(FaBars)`
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
-  margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
+  margin-right: 24px;
+  
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -58,9 +53,7 @@ export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
+   
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -76,8 +69,8 @@ export const NavBtnLink = styled(Link)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-  /* Second Nav */
   margin-left: 24px;
+
   &:hover {
     transition: all 0.2s ease-in-out;
     background: #fff;
