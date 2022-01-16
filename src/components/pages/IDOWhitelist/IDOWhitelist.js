dde37684486c116d05481c5094c190ac957759a4
Casper Yonel <yonelcasper@gmail.com>
2022-01-15 18:15:19 -0700

    updated

--- src/components/pages/IDOWhitelist/IDOWhitelist.js
+++ src/components/pages/IDOWhitelist/IDOWhitelist.js
@@ -3,9 +3,13 @@ import idowhitelist from "./idowhitelist.scss"
 const IDOWhitelist = () => {
     return (
         <div className="outer">
-              <div className="left">
-                  <div className="box-left">
-                      <div className="box-left-1"></div>
+              <div className="box-left-1">
+                  <div className="box-left-2">
+                      <div className="box-left-3">
+                          <div className="box-left-4">
+                              
+                          </div>
+                      </div>
                   </div>
               </div>
               <div className="right">Right</div>
