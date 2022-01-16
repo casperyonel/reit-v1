ff98a53f78524baab38f26aa947e23de2863e55e
Casper Yonel <yonelcasper@gmail.com>
2022-01-16 16:56:42 -0700

    updated

--- src/components/pages/IDOWhitelist/IDOWhitelist.js
+++ src/components/pages/IDOWhitelist/IDOWhitelist.js
@@ -6,9 +6,17 @@ const IDOWhitelist = () => {
               <div className="box-left-1">
                   <div className="box-left-2">
                       <div className="box-left-3">
-                          <div className="box-left-4">
-
+                          <div className="box-left-4a">
+                                <div className="top-4a">
+                                    <p>Select Purchase Amount:</p>
+                                </div>
+                                <div className="middle1-4a"></div>
+                                <div className="middle2-4a"></div>
+                                <div className="bottom-4a"></div>
                           </div>
+                          <div className="box-left-4b">
+                                    
+                                </div>
                       </div>
                   </div>
               </div>
