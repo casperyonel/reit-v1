f9c937f2761c31445639dd2f1817072ea8615a50
Casper Yonel <yonelcasper@gmail.com>
2022-01-17 19:05:08 -0700

    sun

--- src/components/pages/IDOWhitelist/IDOWhitelist.js
+++ src/components/pages/IDOWhitelist/IDOWhitelist.js
@@ -5,11 +5,9 @@ import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 
 const IDOWhitelist = () => {
-    
     const { search } = useLocation()
-    // Pulls query string from URL. 
     const { referrer } = queryString.parse(search)
-    // Parse id off query. 
+    // // Pulls query string from URL and then parses id off query. 
     console.log(search, referrer)
     
     useEffect(() => {
@@ -17,8 +15,25 @@ const IDOWhitelist = () => {
        .then(response => console.log(response.data))
        .catch(error => console.log(error))
     }, [])
+    // Change axios.put before deploying. Consider a seperate Heroku for just axios calls.
+    // Async await?
     
     
+    
+    const [order, setOrder] = useState('')
+
+
+
+
+
+
+    
+    
+    // Upon click of submit, if referrer id is empty, then insert into DB with new wallet_id and 
+    // address, and class bonded.
+    
+    
+    // Upon click of submit, update the conversion_counter of the query id to conversion_counter. 
 
     return (
         <div className="outer">
@@ -39,6 +54,8 @@ const IDOWhitelist = () => {
                                 
                                 <div className="middle1-4a">
 
+                                 
+
                                 <form action="click">
                                     <span className='btn-class-a'>
                                         <label className='class-label'htmlFor="class-a">Class A:</label>
@@ -46,7 +63,7 @@ const IDOWhitelist = () => {
                                         <label className='class-label'htmlFor="class-a">DAI</label>
                                     </span>
                                 </form>
-
+                                
                                 </div>
                                 
                                 <div className="middle2-4a">
@@ -57,7 +74,19 @@ const IDOWhitelist = () => {
                                         <label className='class-label'htmlFor="class-a">1,000</label>
                                         <label className='class-label'htmlFor="class-a">DAI</label>
                                     </span>
+                                     
                                 </form>
+                                
+
+                                
+                                    
+                                
+
+
+
+
+
+
 
                                     
                                 </div>
@@ -67,8 +96,15 @@ const IDOWhitelist = () => {
                                 </div> 
                           </div>
                           <div className="box-left-4b">
-                                    
+                                <div className="top-4b">
+                                    <button id='submit-btn'>Submit Order</button>
                                 </div>
+                                <div className="middle1-4b">
+                                 
+                                </div>
+                                <div className="middle2-4b"></div>
+                                <div className="bottom-4b"></div>       
+                         </div>
 
                                 
                       </div>
@@ -78,6 +114,7 @@ const IDOWhitelist = () => {
                   <div className="box-right-2">
                       <div className="box-right-3">
                           <div className="slot"></div>
+                          {/* Make this a seperate component */}
                           <div className="slot"></div>
                           <div className="slot"></div>
                           <div className="slot"></div>
