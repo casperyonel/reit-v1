8959f24d13117d3e7e5fa1ddc427cca7cb2faa22
Casper Yonel <yonelcasper@gmail.com>
2022-01-19 10:26:06 -0700

    2

--- src/components/pages/IDOWhitelist/Pricingbox.js
+++ src/components/pages/IDOWhitelist/Pricingbox.js
@@ -3,10 +3,30 @@
 const Pricingbox = () => {
     return (
         <div className="pricing-box-1">
-            <div className="pricing-box-1-top"></div>           
-            <div className="pricing-box-1-middle1"></div>           
-            <div className="pricing-box-1-middle2"></div>           
-            <div className="pricing-box-1-bottom"></div>           
+            <div className="pricing-box-1-top">
+                
+                <div className="pricing-box-1-top-container">
+                    Refreshes in 8 seconds.
+                </div>
+
+                <div className="pricing-box-1-top-text">
+                    <span>Hello</span>
+                    <span>500</span>
+                </div>
+
+            </div>           
+            <div className="pricing-box-1-middle1">
+                <span>Hello</span>
+                <span>500</span>
+            </div>           
+            <div className="pricing-box-1-middle2">
+                <span>Hello</span>
+                <span>500</span>
+            </div>           
+            <div className="pricing-box-1-bottom">
+                <span>Hello</span>
+                <span>500</span>
+            </div>           
         </div>
     )
 }
