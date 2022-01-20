1ebca0a7eb3728517dd1b5b8102fca726ed750cf
Casper Yonel <yonelcasper@gmail.com>
2022-01-20 11:23:29 -0700

    io price

--- src/components/pages/IDOWhitelist/Pricingbox.js
+++ src/components/pages/IDOWhitelist/Pricingbox.js
@@ -3,50 +3,30 @@ import { useEffect, useState } from "react"
 
 const Pricingbox = ({ stats, setStats }) => {
 
-    // Current IDO Price calculation:
-    const [currentIDOPrice, setCurrentIDOPrice] = useState(20)
-    let [timer, setTimer] = useState(8)
- 
+    let [timer, setTimer] = useState(10) // Countdown (1/2)
     localStorage.setItem("wallet_address", stats.wallet_address)
-    function updateNewStats() {
-        axios.put('http://localhost:3000/updateStats', ({ wallet_address: localStorage.getItem('wallet_address') }))
-            .then(response => {
-                console.log(response)
-                setStats( {
-                    click_counter: response.data[0][0].click_counter,
-                    conversion_counter: response.data[0][0].conversion_counter,
-                    link: response.data[0][0].link,
-                    wallet_address: response.data[0][0].wallet_address
-                } )
-            }).catch(err => console.log(err))
-    }
-    // useEffect(() => {
-        
-    //     const interval = setInterval(updateNewStats, 10000) // For updating stats
-    //     return () => {
-    //         clearInterval(interval)
-             
-    //     }
-    // }, [])
 
     useEffect(() => {
         const interval = setInterval(() => setTimer(timer - 1), 1000)
         if (timer === 0) {
+            async function updateNewStats() {
+                await axios.put('http://localhost:3000/updateStats', ({ wallet_address: localStorage.getItem('wallet_address') }))
+                    .then(response => {
+                        setStats({
+                            click_counter: response.data[0][0].click_counter,
+                            conversion_counter: response.data[0][0].conversion_counter,
+                            link: response.data[0][0].link,
+                            wallet_address: response.data[0][0].wallet_address,
+                            ido_price: (20 - (Math.floor(response.data[0][0].click_counter) * 0.01) - (Math.floor(response.data[0][0].conversion_counter) * 1)).toFixed(2)
+                        })
+                    }).catch(err => console.log(err))
+            }
             updateNewStats()
-            setTimer(10) // Coutdown length
+            setTimer(10) // Coutdown (2/2)
         }
-        return () => {
-            clearInterval(interval)
-        }
+        return () => clearInterval(interval)
     }, [timer]) // Rendering useEffect every seconds, since this is [timer]
-
-    useEffect(() => {
-        function updateCurrentIDOPrice() {
-            setCurrentIDOPrice((20 - (Number(stats.click_counter) * 0.01) - (Number(stats.conversion_counter) * 1)))
-        }
-        updateCurrentIDOPrice()
-    }, [currentIDOPrice])
-
+    
     return (
         <div className="pricing-box-1">
             <div className="pricing-box-1-top">
@@ -70,7 +50,7 @@ const Pricingbox = ({ stats, setStats }) => {
             </div>           
             <div className="pricing-box-1-bottom">
                 <span>Your current IDO price</span>
-                <span>{currentIDOPrice}</span>
+                <span>{stats.ido_price}</span>
             </div>           
         </div>
     )
