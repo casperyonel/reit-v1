868e30f341ad932b6685e1c393906654936fdfed
Casper Yonel <yonelcasper@gmail.com>
2022-01-28 10:46:32 -0700

    containers

--- src/components/pages/homepage/Homepage.js
+++ src/components/pages/homepage/Homepage.js
@@ -1,29 +1,41 @@
-import homepage from "./homepage.scss"
+import "./homepage.scss"
 
 
 
 const Homepage = () => {
     return (
-     
-            <div className="houter">
-                <div className="hbox-right-1">
-                    <div className="hbox-right-2">
-                        
-                        
-                        <h1 id='come-on'>Perpetuals, decentralized</h1>
-
+            <div className="outer-main">
+                
+                <div className="outer-top">
+                    <div className="outer-top-right-1">
+                        <div className="outer-top-right-2"></div>
+                    </div>
+                    <div className="outer-top-left-1">
+                        <div className="outer-top-left-2"> </div>     
                     </div>
                 </div>
 
-                <div className="hbox-left-1">
-                <div className="hbox-left-2">
-                        
+
+                <div className="outer-middle">
+                    <div className="outer-middle-right-1">
+                        <div className="outer-middle-right-2"></div>
+                    </div>
+                    <div className="outer-middle-left-1">
+                        <div className="outer-middle-left-2"> </div>     
+                    </div>
+                </div>
+
+                <div className="outer-bottom">
+                    <div className="outer-bottom-right-1">
+                        <div className="outer-bottom-right-2"></div>
+                    </div>
+                    <div className="outer-bottom-left-1">
+                        <div className="outer-bottom-left-2"> </div>     
                     </div>
                 </div>
 
                 
             </div>
-    
     )
 }
 
