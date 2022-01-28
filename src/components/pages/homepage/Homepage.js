728b841fa8ea27eaacc73247ee57cfc3c46ea289
Casper Yonel <yonelcasper@gmail.com>
2022-01-28 14:22:49 -0700

    trying

--- src/components/pages/homepage/Homepage.js
+++ src/components/pages/homepage/Homepage.js
@@ -7,34 +7,34 @@ const Homepage = () => {
             <div className="outer-main">
                 
                 <div className="outer-top">
-                    <div className="outer-top-right-1">
-                        <div className="outer-top-right-2">
+                    <div className="outer-top-left-1">
+                        <div className="outer-top-left-2">
                             <h1 className="top-header-text">REIT DAO is monopoly, </h1>
                             <h2 className="top-subheader-text">REIT DAO is monopoly, </h2>
 
                         </div>
                     </div>
-                    <div className="outer-top-left-1">
-                        <div className="outer-top-left-2"> </div>     
+                    <div className="outer-top-right-1">
+                        <div className="outer-top-right-2"> </div>     
                     </div>
                 </div>
 
 
                 <div className="outer-middle">
-                    <div className="outer-middle-right-1">
-                        <div className="outer-middle-right-2"></div>
-                    </div>
                     <div className="outer-middle-left-1">
-                        <div className="outer-middle-left-2"> </div>     
+                        <div className="outer-middle-left-2"></div>
+                    </div>
+                    <div className="outer-middle-right-1">
+                        <div className="outer-middle-right-2"> </div>     
                     </div>
                 </div>
 
                 <div className="outer-bottom">
-                    <div className="outer-bottom-right-1">
-                        <div className="outer-bottom-right-2"></div>
-                    </div>
                     <div className="outer-bottom-left-1">
-                        <div className="outer-bottom-left-2"> </div>     
+                        <div className="outer-bottom-left-2"></div>
+                    </div>
+                    <div className="outer-bottom-right-1">
+                        <div className="outer-bottom-right-2"> </div>     
                     </div>
                 </div>
 
