1f6b77c0449769f86f85d71bd34b1e63afb6fd72
Casper Yonel <yonelcasper@gmail.com>
2022-02-14 15:34:46 -0700

    updated nft contract

--- contracts/veNFT.sol
+++ contracts/veNFT.sol
@@ -7,14 +7,32 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-
 contract veNFT is ERC721 {
+    
+    
+    Struct Info {
+        lockerId uint;
+        tokenLocked address;
+        amount uint;
+        expiryDate uint;
+    }
+    
+    
     using Counters for Counters.Counter;
     Counters.Counter private _tokensIds;
 
-    constructor() public ERC721("NFT Locker 1", "veNFT") { }
+    constructor() public ERC721(, ) { }
 
     function mintNFT(address wallet, string memory tokenURI) public returns (uint256) {
+        
+        // I would use a struct and a mapping of the unique token id to the strict object
+        info = Info ({
+            lockerId: _lockerId,
+            tokenLocked: _token,
+            amount: _amount,
+            expiryDate: block.timestamp + lockUpTime
+        });
+
         _tokensIds.increment();
 
         uint newItemId = _tokensIds.current();
@@ -23,6 +41,8 @@ contract veNFT is ERC721 {
 
         return newItemId;
     }
+
+    // write another function public view returns bool if token id is expired
 
 }
 
