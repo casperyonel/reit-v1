8151cfe287602e80162349d30529d6f93fe4d0d5
Casper Yonel <yonelcasper@gmail.com>
2022-02-14 16:10:30 -0700

    1

--- contracts/veNFT.sol
+++ contracts/veNFT.sol
@@ -9,44 +9,44 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract veNFT is ERC721 {
     
+    mapping(uint => Info) public nftInfo;
     
     Struct Info {
         lockerId uint;
         tokenLocked address;
         amount uint;
         expiryDate uint;
+        lastTime uint;
     }
 
-    mapping(uint => Info) public nftInfo;
-    
-    
     using Counters for Counters.Counter;
     Counters.Counter private _tokensIds;
 
-    constructor() public ERC721(, ) { }
+    constructor() public ERC721(, ) { } // EDIT
 
     function mintNFT(address wallet, string memory tokenURI, uint _lockerId, ERC20 _token, uint _amount, uint lockUpTime) public returns (uint256) {
         _tokensIds.increment();
-
         uint newItemId = _tokensIds.current();
         _mint(wallet, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
 
         nftInfo[ newItemId ] = Info({ 
             lockerId: _lockerId,
             tokenLocked: _token,
             amount: _amount,
-            expiryDate: block.timestamp + lockUpTime
+            expiryDate: uint32(block.timestamp + lockUpTime),
+            lastTime: uint32(block.timestamp)
         });
 
-        
+        return newItemId;
     }
 
     // write another function public view returns bool if token id is expired
 
     // where is string memory tokenURI coming from?
 
+    function status(uint _tokenId) public view returns (Info) {
+        return nftInfo[ _tokenId ]; 
+    }
 }
 
