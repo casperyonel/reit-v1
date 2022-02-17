4b036b729f929b566b7158dffd547a8608c7b0f8
Casper Yonel <yonelcasper@gmail.com>
2022-02-17 12:37:18 -0700

    1

--- contracts/TreasuryMILv4.sol
+++ contracts/TreasuryMILv4.sol
@@ -10,7 +10,10 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract TreasuryMILv4 {
 
     // -- Events --
-    event Deposit( address indexed wallet, address indexed token, uint amount );
+    event Deposit( address indexed token, uint amount, uint value );
+    event CreateDebt( address indexed debtor, address indexed token, uint amount, uint value );
+
+
 
     // -- Accepted tokens -- 
     ERC20 public constant dai = ERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa); // Kovan
