3a8539fd73425894f81a328c189e806a60786a69
Casper Yonel <yonelcasper@gmail.com>
2022-02-17 17:01:30 -0700

    1

---
+++ contracts/Interfaces/IMIL.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./IERC20.sol";
+
+interface IMIL is IERC20 {
+    function mint(address account_, uint256 amount_) external;
+
+    function burn(uint256 amount) external;
+
+    function burnFrom(address account_, uint256 amount_) external;
+}
